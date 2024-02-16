76c3a382bfa09b3aca1da0cf43bf3b513987af28
Iago Núñez Lourés <113685970+kat-lon@users.noreply.github.com>
2024-02-16 13:31:09 +0100

    Update csv_json.py

---
+++ csv_json/csv_json.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def convert_to_dataframe(archivo):
+    return pd.read_csv(archivo) if archivo[-1:-4] == "csv" else pd.read_json(archivo)
+
+def json_to_csv(dataframe):
+    return dataframe.to_json()
+
+def csv_to_json(dataframe):
+    return dataframe.to_csv()
+
+def get_csv_info(archivo_csv):
+    return archivo_csv.info()
